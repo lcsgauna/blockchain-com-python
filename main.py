c34a1941dd61d4469a9f1bebffc6e2454608bce0
Lucas <lucas794@hotmail.com>
2019-10-31 23:18:39 -0400

    Subindo Projeto

---
+++ main.py
@@ -0,0 +1,10 @@
+from blockchain import Blockchain
+
+if __name__ == '__main__':
+    blockchain = Blockchain()
+
+    blockchain.add_new_block('Primeiro bloco')
+    blockchain.add_new_block('Blockchain e top')
+    blockchain.add_new_block('Mais uma vez')
+
+    print(blockchain.get_all())
